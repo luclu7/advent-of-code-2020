710d5a57268044c72c8d1765af558f97969be0e7
Luclu7 <me@luclu7.fr>
2020-12-07 12:38:03 +0100

    day 6 part 2 + readme

--- day6/day6.ts
+++ day6/day6.ts
@@ -2,17 +2,35 @@ import * as fs from "fs";
 import * as chalk from "chalk";
 
 const data = fs.readFileSync('input.txt').toString();
-function part1(){
-    let owo = data.split("\r\n\r\n");
-    let counter: number = 0;
+let parsedInput = data.split("\r\n\r\n");
 
-    owo.forEach((element, index) => {
+function part1(){
+    let counter: number = 0;
+    parsedInput.forEach((element) => {
         const numberOfMatches = element.split("\r\n").join("").split("").filter((v, i, a) => a.indexOf(v) === i).length
-        console.log(numberOfMatches)
+        //console.log(numberOfMatches)
         counter = counter+numberOfMatches;
 
     })
     console.log("(1) Number of matches: "+chalk.green(counter))
 }
 
+function part2(){
+    let counter: number = 0;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    parsedInput.forEach((element) => {
+        const numberOfMembers = element.split("\r\n").length
+        for (const alphaKey in alphabet) {
+            const regex = new RegExp(alphabet[alphaKey],"g");
+            const matches: array = element.match(regex);
+            if(matches !== null && matches.length/numberOfMembers >= 1){
+                //console.log(chalk.red(matches.length/numberOfMembers))
+                counter = counter+(matches.length)/numberOfMembers
+            }
+        }
+    })
+    console.log("(2) Number of matches: "+chalk.green(counter))
+}
+
 part1()
+part2()
